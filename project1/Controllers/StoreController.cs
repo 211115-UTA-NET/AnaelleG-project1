0cef66bd7142200934b4c20dc4a4c485793bafdf
Anaelle <anaellegauthier2@gmail.com>
2022-01-03 01:10:48 -0500

    project1

---
+++ project1/Controllers/StoreController.cs
@@ -0,0 +1,26 @@
+﻿using AnnaFarmerMarket.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace project1.Controllers
+{
+    [ApiController]
+
+    [Route("[Controller]")]
+    public class StoreController : ControllerBase
+    {
+        private readonly IOrder _order;
+        public StoreController (IOrder order)
+        {
+            _order = order; 
+
+        }
+        
+        [HttpPost]
+        public ActionResult PostCustomer(int customerID, string firstName, string lastName, string address, string city, string state, int zipcode)
+        {
+            _order.SqlOrderInformation(customerID,firstName,lastName,address,city,state,zipcode);
+            return StatusCode(200);
+
+        }
+    }
+}
---
+++ project1/Program.cs
@@ -0,0 +1,31 @@
+using AnnaFarmerMarket.Data;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = await File.ReadAllTextAsync("C:/Users/15612/Revature-files/extra-code-stuff/string_CONNECTION.txt");
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IOrder>(sp => new OrderClass(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
